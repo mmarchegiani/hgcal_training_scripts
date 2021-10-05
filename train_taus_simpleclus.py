9d72776a3ab9bd715f0c915ff67fe714a2d33ac5
Thomas Klijnsma <tklijnsm@gmail.com>
2021-10-04 21:12:13 -0500

    training script for the simple clustering loss with distance-to-center loss

---
+++ train_taus_simpleclus.py
@@ -0,0 +1,192 @@
+import os, os.path as osp
+from time import strftime
+import tqdm
+import torch
+from torch_geometric.data import DataLoader
+import argparse
+import numpy as np
+
+from torch_cmspepr.dataset import TauDataset
+from torch_cmspepr.gravnet_model import GravnetModel
+import torch_cmspepr.objectcondensation as oc
+from lrscheduler import CyclicLRWithRestarts
+
+torch.manual_seed(1009)
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dry', action='store_true', help='Turn off checkpoint saving and run limited number of events')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Print more output')
+    parser.add_argument('--ckptdir', type=str)
+    args = parser.parse_args()
+    if args.verbose: oc.DEBUG = True
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device', device)
+
+    n_epochs = 400
+    batch_size = 4
+
+    shuffle = True
+    dataset = TauDataset('data/taus')
+    dataset.blacklist([ # Remove a bunch of bad events
+        'data/taus/110_nanoML_98.npz',
+        'data/taus/113_nanoML_13.npz',
+        'data/taus/124_nanoML_77.npz',
+        'data/taus/128_nanoML_70.npz',
+        'data/taus/149_nanoML_90.npz',
+        'data/taus/153_nanoML_22.npz',
+        'data/taus/26_nanoML_93.npz',
+        'data/taus/32_nanoML_45.npz',
+        'data/taus/5_nanoML_51.npz',
+        'data/taus/86_nanoML_97.npz',
+        ])
+    if args.dry:
+        keep = .0025
+        print(f'Keeping only {100.*keep:.2f}% of events for debugging')
+        dataset, _ = dataset.split(keep)
+        batch_size = 2
+    train_dataset, test_dataset = dataset.split(.8)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle)
+
+    model = GravnetModel(input_dim=9, output_dim=4, k=10 if args.dry else 50).to(device)
+    epoch_size = len(train_loader.dataset)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5, weight_decay=1e-4)
+    scheduler = CyclicLRWithRestarts(optimizer, batch_size, epoch_size, restart_period=400, t_mult=1.1, policy="cosine")
+
+    def loss_fn(out, data, s_c=1., return_components=False):
+        pred_edc = torch.abs(out[:,0])
+        pred_cluster_space_coords = out[:,1:]
+        return oc.calc_simple_clus_space_loss(
+            pred_cluster_space_coords,
+            data.y.long(),
+            data.batch,
+            pred_edc=pred_edc
+            )
+
+    def train(epoch):
+        print('Training epoch', epoch)
+        model.train()
+        scheduler.step()
+        try:
+            pbar = tqdm.tqdm(train_loader, total=len(train_loader))
+            pbar.set_postfix({'loss': '?'})
+            for i, data in enumerate(pbar):
+                data = data.to(device)
+                optimizer.zero_grad()
+                result = model(data.x, data.batch)
+                loss = sum(loss_fn(result, data))
+                loss.backward()
+                optimizer.step()
+                scheduler.batch_step()
+                pbar.set_postfix({'loss': float(loss)})
+                # if i == 2: raise Exception
+        except Exception:
+            print('Exception encountered:', data, ', npzs:')
+            print('  ' + '\n  '.join([train_dataset.npzs[int(i)] for i in data.inpz]))
+            raise
+
+    def test(epoch):
+        N_test = len(test_loader)
+        losses = np.zeros(3)
+        with torch.no_grad():
+            model.eval()
+            for data in tqdm.tqdm(test_loader, total=len(test_loader)):
+                data = data.to(device)
+                result = model(data.x, data.batch)
+                losses += loss_fn(result, data, return_components=True)
+        losses /= N_test
+        print(f'test loss:\n  L = {losses.sum():.2f} (100%)')
+        for loss_component, loss_percentage, label in zip(
+            losses, losses/losses.sum()*100., ['L_att', 'L_rep', 'L_edc']
+            ):
+            print(f'\n    {label} = {loss_component:8.2f} ({loss_percentage:5.2f}%)')
+        return losses.sum()
+
+    ckpt_dir = strftime('ckpts_gravnet_%b%d_%H%M') if args.ckptdir is None else args.ckptdir
+    def write_checkpoint(checkpoint_number=None, best=False):
+        ckpt = 'ckpt_best.pth.tar' if best else 'ckpt_{0}.pth.tar'.format(checkpoint_number)
+        ckpt = osp.join(ckpt_dir, ckpt)
+        if best: print('Saving epoch {0} as new best'.format(checkpoint_number))
+        if not args.dry:
+            os.makedirs(ckpt_dir, exist_ok=True)
+            torch.save(dict(model=model.state_dict()), ckpt)
+
+    min_loss = 1e9
+    for i_epoch in range(n_epochs):
+        train(i_epoch)
+        write_checkpoint(i_epoch)
+        test_loss = test(i_epoch)
+        if test_loss < min_loss:
+            min_loss = test_loss
+            write_checkpoint(i_epoch, best=True)
+
+def debug():
+    oc.DEBUG = True
+    dataset = TauDataset('data/taus')
+    dataset.npzs = [
+        # 'data/taus/49_nanoML_84.npz',
+        # 'data/taus/37_nanoML_4.npz',
+        'data/taus/26_nanoML_93.npz',
+        # 'data/taus/142_nanoML_75.npz',
+        ]
+    for data in DataLoader(dataset, batch_size=len(dataset), shuffle=False): break
+    print(data.y.sum())
+    model = GravnetModel(input_dim=9, output_dim=4)
+    with torch.no_grad():
+        model.eval()
+        out = model(data.x, data.batch)
+    pred_edc = torch.sigmoid(out[:,0])
+    pred_cluster_space_coords = out[:,1:4]
+    out_oc = oc.calc_LV_Lbeta(
+        pred_edc,
+        pred_cluster_space_coords,
+        data.y.long(),
+        data.batch.long()
+        )
+
+def run_profile():
+    from torch.profiler import profile, record_function, ProfilerActivity
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device', device)
+
+    batch_size = 2
+    n_batches = 2
+    shuffle = True
+    dataset = TauDataset('data/taus')
+    dataset.npzs = dataset.npzs[:batch_size*n_batches]
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+    print(f'Running profiling for {len(dataset)} events, batch_size={batch_size}, {len(loader)} batches')
+
+    model = GravnetModel(input_dim=9, output_dim=8).to(device)
+    epoch_size = len(loader.dataset)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-7, weight_decay=1e-4)
+
+    print('Start limited training loop')
+    model.train()
+    with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
+        with record_function("model_inference"):
+            pbar = tqdm.tqdm(loader, total=len(loader))
+            pbar.set_postfix({'loss': '?'})
+            for i, data in enumerate(pbar):
+                data = data.to(device)
+                optimizer.zero_grad()
+                result = model(data.x, data.batch)
+                loss = loss_fn(result, data)
+                print(f'loss={float(loss)}')
+                loss.backward()
+                optimizer.step()
+                pbar.set_postfix({'loss': float(loss)})
+    print(prof.key_averages().table(sort_by="cpu_time", row_limit=10))
+    # Other valid keys:
+    # cpu_time, cuda_time, cpu_time_total, cuda_time_total, cpu_memory_usage,
+    # cuda_memory_usage, self_cpu_memory_usage, self_cuda_memory_usage, count
+
+if __name__ == '__main__':
+    pass
+    main()
+    # debug()
+    # run_profile()
